3ea39b6a506fa550f1a2ce21ba80a92546bc993b
lucianvannacht <165604906+lucianvannacht@users.noreply.github.com>
2024-04-30 12:47:48 +0200

    Update miner_utils.py

--- omega/miner_utils.py
+++ omega/miner_utils.py
@@ -435,8 +435,6 @@ async def search_and_embed_videos(original_query: str, query: str, num_videos: i
     results = filtered_results
     
     video_metas = []
-    num_videos = 25
-
     # fetch random proxy ip
     if len(PROXY_URLS):
         proxy_url = random.choice(PROXY_URLS)
